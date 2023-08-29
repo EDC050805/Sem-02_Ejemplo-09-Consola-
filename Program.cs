845758e5a7871854208a1d4ed21e6da05e70703c
Elias Alejandro Diaz Cahuas <N00369765@upn.pe>
2023-08-28 23:13:23 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int num1, num2, num3;
+
+Console.WriteLine("\tPrograma para saber el mayor de tres números");
+Console.WriteLine("\t********************************************\n");
+Console.Write("Inserte el primer número: ");
+num1 = int.Parse(Console.ReadLine());
+Console.Write("Inserte el segundo número: ");
+num2 = int.Parse(Console.ReadLine());
+Console.Write("Inserte el tercer número: ");
+num3 = int.Parse(Console.ReadLine());
+
+if (num1 != num2 && num1 != num3 && num2 != num3)
+{
+    if (num1 > num2 && num1 > num3)
+        Console.WriteLine("El número mayor es: {0}", num1);
+    if (num2 > num1 && num2 > num3)
+        Console.WriteLine("El número mayor es: {0}", num2);
+    if (num3 > num1 && num3 > num2)
+        Console.WriteLine("El número mayor es: {0}", num3);
+}
+else
+    Console.WriteLine("¡Error! Debe colocar tres números diferentes entre sí");
+
+Console.ReadKey();
